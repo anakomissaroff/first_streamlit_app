24e1f24110a40cb0479ef8d3480186f17184ba9d
anakomissaroff <69453783+anakomissaroff@users.noreply.github.com>
2022-12-07 15:08:47 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -21,12 +21,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#Add a text entry box and send the input to fruityvice as part of the API Call
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 #New section to display api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
 # normalize json file
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # to present the normalized json file in streamlit
 streamlit.dataframe(fruityvice_normalized)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
