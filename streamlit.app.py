5efffd69082205af99747d21beffd65e2debdbe4
anakomissaroff <69453783+anakomissaroff@users.noreply.github.com>
2022-12-08 13:36:32 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -62,7 +62,7 @@ if streamlit.button('Get Fruit List'):
 def insert_row_snowflake(new_fruit):
    with my_cnx.cursor() as my_cur:
       my_cur.execute("insert into fruit_load_list values ('"+ new_fruit + "')")
-      return "Thanks for adding" + new_fruit
+      return "Thanks for adding " + new_fruit
    
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
@@ -70,4 +70,4 @@ if streamlit.button('Add a fruit to the list'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)     
       
-streamlit.write('Thanks for adding ', add_my_fruit)
+#streamlit.write('Thanks for adding ', add_my_fruit)
